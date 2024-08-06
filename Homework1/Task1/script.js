dd58d6ca186c32cd30c359cfab30a377b4ab1c8b
Vilovinto <vladvierboviy@gmail.com>
2024-08-06 19:38:33 +0300

    complete Task1

--- Homework1/Task1/script.js
+++ Homework1/Task1/script.js
@@ -1,26 +1,26 @@
-let text1 = 'hello'
-console.log(text1)
-let text2 = 'owu'
-console.log(text2)
-let text3 = 'com'
-console.log(text3)
-let text4 = 'ua'
-console.log(text4)
-let num1 = 1
-console.log(num1)
-let num2 = 10
-console.log(num2)
-let num3 = -999
-console.log(num3)
-let num4 = 123
-console.log(num4)
-let num5 = 3.14
-console.log(num5)
-let num6 = 2.7
-console.log(num6)
-let num7 = 16
-console.log(num7)
-let text5 = true
-console.log(text5)
-let text6 = false
-console.log(text6)
+let text1 = 'hello';
+console.log(text1);
+let text2 = 'owu';
+console.log(text2);
+let text3 = 'com';
+console.log(text3);
+let text4 = 'ua';
+console.log(text4);
+let num1 = 1;
+console.log(num1);
+let num2 = 10;
+console.log(num2);
+let num3 = -999;
+console.log(num3);
+let num4 = 123;
+console.log(num4);
+let num5 = 3.14;
+console.log(num5);
+let num6 = 2.7;
+console.log(num6);
+let num7 = 16;
+console.log(num7);
+let text5 = true;
+console.log(text5);
+let text6 = false;
+console.log(text6);
