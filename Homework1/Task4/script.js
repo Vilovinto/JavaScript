ccd2b70a9ed407788c00ddcef574044784607e7c
Vilovinto <vladvierboviy@gmail.com>
2024-08-06 19:52:59 +0300

    complete Task4

---
+++ Homework1/Task4/script.js
@@ -0,0 +1,8 @@
+let firstName = prompt();
+console.log(firstName);
+let lastName = prompt();
+console.log(lastName);
+let year = prompt();
+console.log(year);
+let sum = `${firstName} ${lastName} ${year}`;
+console.log(sum);
