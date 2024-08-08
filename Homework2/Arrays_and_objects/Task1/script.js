2aa15c83d293b566d97db21e0cee76a2b12117b5
Vilovinto <vladvierboviy@gmail.com>
2024-08-09 01:08:47 +0300

    update Task1

--- Homework2/Arrays_and_objects/Task1/script.js
+++ Homework2/Arrays_and_objects/Task1/script.js
@@ -1,2 +1,12 @@
 let array = ['first', 122, 'one', false, 20, 'street', 4, true, 'second', 11];
 console.log(array);
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+console.log(array[3]);
+console.log(array[4]);
+console.log(array[5]);
+console.log(array[6]);
+console.log(array[7]);
+console.log(array[8]);
+console.log(array[9]);
