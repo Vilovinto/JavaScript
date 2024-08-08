54bc5b4a162eabaaa738e0ce2ae7da1056603073
Vilovinto <vladvierboviy@gmail.com>
2024-08-09 01:22:02 +0300

    complete Task5

---
+++ Homework2/Arrays_and_objects/Task5/script.js
@@ -0,0 +1,10 @@
+let temperature = [
+    {morning: 19, afternoon : 25, evening: 17},
+    {morning: 18, afternoon : 26, evening: 19},
+    {morning: 16, afternoon : 22, evening: 15},
+    {morning: 13, afternoon : 20, evening: 15},
+    {morning: 17, afternoon : 26, evening: 17},
+    {morning: 10, afternoon : 19, evening: 12},
+    {morning: 15, afternoon : 26, evening: 24},
+];
+console.log(temperature);
