27ce7f5610c22858da38142ce0ebd8e7200b63e1
Vilovinto <vladvierboviy@gmail.com>
2024-08-09 01:35:05 +0300

    start Logical_ramifications and complete Task1

---
+++ Homework2/Logical_ramifications/Task1/script.js
@@ -0,0 +1,23 @@
+let a = 1;
+if (a !== 0) {
+    console.log('Вірно')
+}
+else {
+    console.log('Невірно')
+}
+
+let b = 0;
+if (b !== 0) {
+    console.log('Вірно')
+}
+else {
+    console.log('Невірно')
+}
+
+let c = -3;
+if (c !== 0) {
+    console.log('Вірно')
+}
+else {
+    console.log('Невірно')
+}
