e236d40110547c3f642b519d681daa3b2b3571d8
Vilovinto <vladvierboviy@gmail.com>
2024-08-08 19:44:24 +0300

    complete Task2

---
+++ Homework2/Arrays_and_objects/Task2/script.js
@@ -0,0 +1,6 @@
+let book1 = {title: 'The Alchemist', pageCount: 288, genre: 'novel'};
+console.log(book1);
+let book2 = {title: 'The Hunger Games', pageCount: 352, genre: 'dystopia novel'};
+console.log(book2);
+let book3 = {title: 'Harry Potter and the Deathly Hallows', pageCount: 640, genre: 'novel'};
+console.log(book3);
