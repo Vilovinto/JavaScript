60a52f3dea609f0e715f89f3980288b1b9b33f6a
Vilovinto <vladvierboviy@gmail.com>
2024-08-08 19:49:42 +0300

    complete Task3

---
+++ Homework2/Arrays_and_objects/Task3/script.js
@@ -0,0 +1,6 @@
+let book1 = {title: 'The Alchemist', pageCount: 288, authors: ['Paulo Coelho', 76]};
+console.log(book1);
+let book2 = {title: 'The Hunger Games', pageCount: 352, authors: ['Suzanne Collins', 61]};
+console.log(book2);
+let book3 = {title: 'Harry Potter and the Deathly Hallows', pageCount: 640, authors: ['J. K. Rowling', 59]};
+console.log(book3);
