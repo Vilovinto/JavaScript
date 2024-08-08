a990c64b31a1bbaa3a03091ba84707a3e0fb41ca
Vilovinto <vladvierboviy@gmail.com>
2024-08-09 00:56:20 +0300

    complete Task4

---
+++ Homework2/Arrays_and_objects/Task4/script.js
@@ -0,0 +1,21 @@
+let user = [{name: 'Vlad', username: 'Vilovinto', password: 4927},
+    {name: 'Sergiy', username: 'Sergey344', password: 5893},
+    {name: 'Iryna', username: 'Iryna112', password: 5236},
+    {name: 'Andrii', username: 'Gilah', password: 7523},
+    {name: 'Artem', username: 'Aeoris', password: 2367},
+    {name: 'Arthur', username: 'Zelenyi', password: 9651},
+    {name: 'Illia', username: 'Laktioff', password: 6622},
+    {name: 'Ostap', username: 'Haluh', password: 7123},
+    {name: 'Volodimir', username: 'Vertam', password: 1214},
+    {name: 'Diana', username: 'Bilax', password: 6351}];
+console.log(user);
+console.log(user[0].password);
+console.log(user[1].password);
+console.log(user[2].password);
+console.log(user[3].password);
+console.log(user[4].password);
+console.log(user[5].password);
+console.log(user[6].password);
+console.log(user[7].password);
+console.log(user[8].password);
+console.log(user[9].password);
