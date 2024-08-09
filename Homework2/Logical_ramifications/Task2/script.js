e7d05c8377c34cbda54efb92460cf822d0e1b7f0
Vilovinto <vladvierboviy@gmail.com>
2024-08-09 14:07:30 +0300

    complete Task2

---
+++ Homework2/Logical_ramifications/Task2/script.js
@@ -0,0 +1,17 @@
+let time = 0;
+switch (true){
+    case (0<=time && time<15):
+        console.log('Перша чверть години')
+        break;
+    case (15<=time && time<30):
+        console.log('Друга чверть години')
+        break;
+    case (30<=time && time<45):
+        console.log('Третя чверть години')
+        break;
+    case (45<=time && time<60):
+        console.log('Четверта чверть години')
+        break;
+    default:
+        console.log('Невірно')
+}
