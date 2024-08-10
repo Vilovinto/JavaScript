e5ef3a79e6a59631a67cecad474747427c0a3c67
Vlad <vladvierboviy@gmail.com>
2024-08-10 13:02:13 +0300

    update Task3

--- Homework2/Arrays_and_objects/Task3/script.js
+++ Homework2/Arrays_and_objects/Task3/script.js
@@ -1,6 +1,6 @@
-let book1 = {title: 'The Alchemist', pageCount: 288, authors: ['Paulo Coelho', 76]};
+let book1 = {title: 'The Alchemist', pageCount: 288, authors: [{name: 'Paulo Coelho', age: 76}]};
 console.log(book1);
-let book2 = {title: 'The Hunger Games', pageCount: 352, authors: ['Suzanne Collins', 61]};
+let book2 = {title: 'The Hunger Games', pageCount: 352, authors: [{name: 'Suzanne Collins', age: 61}]};
 console.log(book2);
-let book3 = {title: 'Harry Potter and the Deathly Hallows', pageCount: 640, authors: ['J. K. Rowling', 59]};
+let book3 = {title: 'Harry Potter and the Deathly Hallows', pageCount: 640, authors: [{name: 'J. K. Rowling', age: 59}]};
 console.log(book3);
