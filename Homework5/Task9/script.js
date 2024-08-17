59eaac09a46e0f273e227e4fd731ec9b6a745a65
Vilovinto <vladvierboviy@gmail.com>
2024-08-17 13:55:58 +0300

    complete Task9

---
+++ Homework5/Task9/script.js
@@ -0,0 +1,6 @@
+const mas = (users) =>{
+    for (const user of users){
+        document.write(`<div>${user.id} ${user.name} ${user.age}</div>`);
+    }
+}
+mas([{id: 1, name: 'Vlad', age: 20}, {id: 2, name: 'John', age: 40}, {id: 3, name: 'Anton', age: 35}]);
