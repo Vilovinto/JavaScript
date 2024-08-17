722d942a13f1b15f962696dffef12feeb15bcfd6
Vilovinto <vladvierboviy@gmail.com>
2024-08-17 14:08:51 +0300

    complete Task10

---
+++ Homework5/Task10/script.js
@@ -0,0 +1,10 @@
+const arrmin = (numbers) =>{
+    let min = numbers [0];
+    for (const number of numbers){
+        if (number < min){
+            min = number;
+        }
+    }
+    return min;
+}
+console.log(arrmin([77, 22, 6, 999, 31]));
