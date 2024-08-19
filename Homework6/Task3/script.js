c3f0659ebdb1c064309481554beb3c2c81273772
Vilovinto <vladvierboviy@gmail.com>
2024-08-20 00:16:01 +0300

    complete Task3

---
+++ Homework6/Task3/script.js
@@ -0,0 +1,8 @@
+let s1 = 'HELLO WORLD';
+let s2 = 'LOREM IPSUM';
+let s3 = 'JAVASCRIPT IS COOL';
+const strings = [s1, s2, s3];
+for (const s of strings) {
+    const str = s.toLowerCase();
+    console.log(str);
+}
