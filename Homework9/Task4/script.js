d1e20c8af2c03abb05bcf1a4d55dbc643990d079
Vilovinto <vladvierboviy@gmail.com>
2024-08-28 23:30:29 +0300

    complete Task4

---
+++ Homework9/Task4/script.js
@@ -0,0 +1,17 @@
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+for (const course of coursesAndDurationArray) {
+    const div = document.createElement('div');
+    const h1 = document.createElement('h1');
+    h1.innerText = course.title
+    const p = document.createElement('p');
+    p.innerText = course.monthDuration;
+    div.append(h1, p);
+    document.body.appendChild(div);
+}
