0d9e99013baf20d2f5c9246e703fba4ffd4e3b78
Vilovinto <vladvierboviy@gmail.com>
2024-09-04 11:13:37 +0300

    corrected mistake

--- Homework10/Task5/script.js
+++
@@ -1,5 +0,0 @@
-const input = document.getElementsByTagName('input')[0];
-const div = document.getElementsByTagName('div')[0];
-input.oninput = function () {
-    div.innerText = + this.value * 2.2;
-};
--- Homework10/Task6/script.js
+++ Homework10/Task6/script.js
@@ -1,13 +1,5 @@
-function addToLocalStorage(arrayName, objToAdd) {
-    const lsItem = localStorage.getItem(arrayName);
-    if (!lsItem) {
-        throw new Error('there is no such array');
-    }
-    const array = JSON.parse(lsItem);
-    if (typeof objToAdd === 'object') {
-        array.push(objToAdd);
-    }
-    localStorage.setItem(arrayName, JSON.stringify(array));
-}
-localStorage.setItem('sessionsList', JSON.stringify([]));
-addToLocalStorage('sessionsList', {});
+const input = document.getElementsByTagName('input')[0];
+const div = document.getElementsByTagName('div')[0];
+input.oninput = function () {
+    div.innerText = + this.value * 2.2;
+};
