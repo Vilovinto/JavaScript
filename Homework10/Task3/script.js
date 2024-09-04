1dff6a132ecb4e8f40453f768a1f1e49aa564339
Vilovinto <vladvierboviy@gmail.com>
2024-09-04 11:53:03 +0300

    complete Task3

--- Homework10/Task3/script.js
+++ Homework10/Task3/script.js
@@ -1,10 +1,10 @@
 const form = document.forms.someForm;
 const div = document.getElementsByTagName('div')[0];
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+form.addEventListener('submit', (i) => {
+    i.preventDefault();
     const name = form.name.value;
     const surname = form.surname.value;
-    const age = form.age.value;
+    const age = +form.age.value;
     let obj = {name, surname, age}
     console.log(obj);
     div.innerText = obj.name + ' ' + obj.surname + ' ' + obj.age;
